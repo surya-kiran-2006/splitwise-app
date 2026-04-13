22538c5c3493301d6e7033a24b0cb637f977739f
surya-kiran-2006 <surya30032006@gmail.com>
2026-04-13 19:34:37 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Routes
